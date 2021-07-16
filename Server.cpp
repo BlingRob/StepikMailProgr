b9e073949e29755d96186e854fadffae957c0702
BlingRob <67543958+BlingRob@users.noreply.github.com>
2021-07-16 19:31:39 +0300

    Update Server.cpp

--- Server.cpp
+++ Server.cpp
@@ -3,7 +3,7 @@
 Server::Server(std::unique_ptr<server_options> data) 
 {
 	point = boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string(data->IP), data->Port);
-	acc = std::make_unique<boost::asio::ip::tcp::acceptor>(service, point);
+	acc = std::unique_ptr<boost::asio::ip::tcp::acceptor>(new boost::asio::ip::tcp::acceptor(service, point));
 	info = std::move(data);
 }
 
