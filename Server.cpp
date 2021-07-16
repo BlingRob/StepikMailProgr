0e0a87b1d8b31b8dee78b1e0ceb238990e2ba77d
BlingRob <67543958+BlingRob@users.noreply.github.com>
2021-07-16 16:23:56 +0300

    Add files via upload

---
+++ Server.cpp
@@ -0,0 +1,28 @@
+#include "Server.h"
+
+Server::Server(std::unique_ptr<server_options> data) 
+{
+	point = boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string(data->IP), data->Port);
+	acc = std::make_unique<boost::asio::ip::tcp::acceptor>(service, point);
+	info = std::move(data);
+}
+
+void Server::run() 
+{
+	start_accept(new Client_Request(service,info->Path));
+	service.run();
+}
+
+void Server::start_accept(Client_Request* req)
+{
+	acc->async_accept(req->socket(), boost::bind(&Server::handle_accept, this, req, boost::placeholders::_1));
+}
+void Server::handle_accept(Client_Request* req, const boost::system::error_code& err)
+{
+	if (!err)
+		req->start();
+	else
+		delete req;
+	start_accept(new Client_Request(service, info->Path));
+	service.run();
+}
---
+++ StepikHttpServer.cpp
@@ -0,0 +1,24 @@
+﻿// StepikHttpServer.cpp: определяет точку входа для приложения.
+//
+
+#include "Headers.h"
+#include "Parser.h"
+#include "Server.h"
+
+
+int main(int getc,char** getv)
+{
+	Parser prs;
+	try
+	{
+		std::unique_ptr<server_options> SrvOpt = prs.CheckCMDParametrs(getc, getv);
+		Server serv(std::move(SrvOpt));
+		serv.run();
+	}
+	catch(std::exception e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	return 0;
+}
